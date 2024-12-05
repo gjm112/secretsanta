2cc4d4bac9d56b247ceb178aa7019c43dade8292
Gregoery J. Matthews <gregorymatthews@Gregorys-MacBook-Pro.local>
2024-12-05 09:57:13 -0600

    function.

---
+++ R/secretsanta.R
@@ -0,0 +1,29 @@
+#Secret santa function goes here
+
+#Input is a character vector of names
+
+secretsanta <- function(names, sean = FALSE){
+
+  #The "sean" method to always create a cycle
+  if (sean){
+  temp <- sample(names)
+  out <- data.frame(secretsanta = temp, recipient = c(temp[-1], temp[1]))
+  return(out)
+  }
+
+  if (!sean){
+    recipient <- names
+
+    while(any(recipient == names)){
+      recipient <- sample(names)
+    }
+
+    out <- data.frame(secretsanta = names, recipient = recipient)
+    return(out)
+  }
+
+
+}
+
+
+
